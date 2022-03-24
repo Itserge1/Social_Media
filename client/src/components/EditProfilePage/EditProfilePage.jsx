d83dc535f8510464955ea48ce9f77c634edd4532
Itserge1 <realitserge@gmail.com>
2022-03-24 18:09:20 -0400

    add use effect for identity check

--- client/src/components/EditProfilePage/EditProfilePage.jsx
+++ client/src/components/EditProfilePage/EditProfilePage.jsx
@@ -14,24 +14,24 @@ const EditProfilePage = () => {
 
     const history = useHistory();
 
-    // useEffect(() => {
-    //     axios.get(`${process.env.REACT_APP_API_LINK}/api/finduser`, { withCredentials: true })
-    //         .then(res => {
-    //             console.log("Your logged in user info", res)
-    //             // res.data.results will contains the info of the user, 
-    //             // that has its id in the cookies. if the user logged in, he will have one. 
-    //             // if not he won't have a cookie therefore no info
-    //             if (res.data.results) {
-    //                 // user have a cookies
-    //                 setLoggedInUser(res.data.results)
-    //                 console.log("ok");
-    //             }
-    //         })
-    //         .catch(err => {
-    //             console.log("Erorr when getting logged in user", err);
-    //             history.push("/");
-    //         })
-    // }, [])
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_LINK}/api/finduser`, { withCredentials: true })
+            .then(res => {
+                console.log("Your logged in user info", res)
+                // res.data.results will contains the info of the user, 
+                // that has its id in the cookies. if the user logged in, he will have one. 
+                // if not he won't have a cookie therefore no info
+                if (res.data.results) {
+                    // user have a cookies
+                    setLoggedInUser(res.data.results)
+                    console.log("ok");
+                }
+            })
+            .catch(err => {
+                console.log("Erorr when getting logged in user", err);
+                history.push("/");
+            })
+    }, [])
 
     // USER INFO ONCHAGEHANDLER
     const onChangeHandler = (event) => {
